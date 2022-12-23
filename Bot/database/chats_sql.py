3d330e7cb24f0f1ebd07558f7dd7b25781dda55f
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-12-23 20:46:38 +0530

    Create chats_sql.py

---
+++ Bot/database/chats_sql.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, UnicodeText, String, BigInteger
+from Bot.database import BASE, SESSION
+
+
+class Chats(BASE):
+    __tablename__ = "chats"
+    __table_args__ = {'extend_existing': True}
+    chat_id = Column(BigInteger, primary_key=True)
+    username = Column(UnicodeText)
+    allow_usage = Column(String)
+
+    def __init__(self, chat_id, username=None, allow_usage="Everyone"):
+        self.chat_id = chat_id
+        self.username = username
+        self.allow_usage = allow_usage
+
+    def __repr__(self):
+        return "<Chat {} {} ({})>".format(self.username, self.allow_usage, self.chat_id)
+
+
+Chats.__table__.create(checkfirst=True)
+
+
+def num_chats():
+    try:
+        return SESSION.query(Chats).count()
+    finally:
+        SESSION.close()
